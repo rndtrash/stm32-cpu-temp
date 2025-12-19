e336bec8c1dd748f99e9ba993606644eeb58e0e7
Ivan Kuzmenko <6745157+rndtrash@users.noreply.github.com>
2025-12-19 04:33:07 +0300

    feat: Отправка пакета об ошибке и базовый парсинг пакетов

---
+++ Core/Inc/packet.hpp
@@ -0,0 +1,68 @@
+#ifndef STM32_CPU_TEMP_PACKET_H
+#define STM32_CPU_TEMP_PACKET_H
+
+#include <cstddef>
+#include <cstdint>
+#include <cstring>
+#include <optional>
+
+constexpr uint16_t UART_PACKET_MAGIC = 0x55AA; // В буфере: 0xAA 0x55
+
+enum class UartPacketTypes : uint8_t {
+    INVALID = 0,
+    ERROR,
+    REQUEST,
+    REQUEST_CONTINUOUS_START,
+    REQUEST_CONTINUOUS_STOP,
+    RESPONSE,
+    TYPES_COUNT
+};
+
+constexpr auto UART_PACKETS_COUNT = static_cast<std::underlying_type_t<UartPacketTypes>>(UartPacketTypes::TYPES_COUNT);
+
+struct UartPacket {
+    uint16_t magic = UART_PACKET_MAGIC;
+    UartPacketTypes type = UartPacketTypes::INVALID;
+
+    std::optional<size_t> serialize(std::byte *buffer, const size_t bufferSize) const {
+        constexpr auto packetSize = sizeof(magic) + sizeof(type);
+        if (bufferSize < packetSize) return std::nullopt;
+
+        // TODO: std::byteswap из C++23 очень хочется
+        buffer[0] = static_cast<std::byte>(magic & 0x00FF);
+        buffer[1] = static_cast<std::byte>((magic & 0xFF00) >> 8);
+        buffer[2] = static_cast<std::byte>(type);
+
+        return packetSize;
+    }
+};
+
+struct UartPacketRequestTemp : UartPacket {
+    explicit UartPacketRequestTemp() : UartPacket() { type = UartPacketTypes::REQUEST; }
+};
+
+struct UartPacketResponseTemp : UartPacket {
+    explicit UartPacketResponseTemp(const float temp) : UartPacket(), temperature(temp) { type = UartPacketTypes::RESPONSE; }
+
+    std::optional<size_t> serialize(std::byte *buffer, const size_t bufferSize) const {
+        if (const auto packetSizeO = UartPacket::serialize(buffer, bufferSize)) {
+            const auto packetSize = packetSizeO.value();
+            if (bufferSize < packetSize + sizeof(temperature)) return std::nullopt;
+
+            const auto temperatureBytes = reinterpret_cast<const std::byte *>(&temperature);
+            std::memmove(buffer + packetSize, temperatureBytes, sizeof(temperature));
+
+            return packetSize + sizeof(temperature);
+        }
+        return std::nullopt;
+    }
+
+    float temperature = .0f;
+};
+
+struct UartPacketError : UartPacket {
+    explicit UartPacketError() : UartPacket() { type = UartPacketTypes::ERROR; }
+    std::optional<size_t> serialize(std::byte *buffer, const size_t bufferSize) const { return UartPacket::serialize(buffer, bufferSize); }
+};
+
+#endif //STM32_CPU_TEMP_PACKET_H
--- Core/Src/app.cpp
+++ Core/Src/app.cpp
@@ -1,18 +1,116 @@
-#include <atomic>
 #include "stm32f0xx_ll_usart.h"
 
 #include <app.h>
+#include <packet.hpp>
 #include <queue.hpp>
 
 static auto queue = ByteQueue<32>();
-bool error = false;
+static bool error = false;
+
+extern CRC_HandleTypeDef hcrc;
+
+enum class PacketParseState {
+    INITIAL = 0,
+    FIRST_MAGIC,
+    SECOND_MAGIC,
+    PACKET_TYPE,
+    CRC32
+};
+
+static bool crcAndTransmit(uint32_t *buffer, const size_t bufferSize, const size_t packetSize) {
+    const auto crc = HAL_CRC_Calculate(&hcrc, buffer, packetSize);
+    if (bufferSize < packetSize + sizeof(crc)) return false;
+
+    auto *byteBuffer = reinterpret_cast<uint8_t *>(buffer);
+    for (size_t i = 0; i < sizeof(crc); i++) {
+        byteBuffer[packetSize + i] = static_cast<uint8_t>((crc & (0xFF << i * 8)) >> (i * 8));
+    }
+
+    const auto sizeFinal = packetSize + sizeof(crc);
+    for (size_t i = 0; i < sizeFinal; i++) {
+        LL_USART_TransmitData8(USART2, byteBuffer[i]);
+
+        const auto start = HAL_GetTick();
+        constexpr auto maxWaitMs = 100;
+        while (!LL_USART_IsActiveFlag_TXE(USART2) && HAL_GetTick() - start < maxWaitMs) {
+        }
+
+        if (HAL_GetTick() - start >= maxWaitMs)
+            return false;
+    }
+
+    return true;
+}
+
+template<typename T>
+[[nodiscard]]
+static bool sendPacket(T &packet) {
+    union {
+        std::array<std::byte, 16> packetBuffer;
+        std::array<uint32_t, 4> packetBuffer32 = {}; // Для выравнивания по границе двойного слова
+    };
+
+    const std::optional<size_t> size = packet.serialize(packetBuffer.data(), packetBuffer.size());
+    if (!size) return false;
+
+    return crcAndTransmit(packetBuffer32.data(), packetBuffer.size(), size.value());
+}
+
+static void parsePacket(const std::byte byte) {
+    static auto state = PacketParseState::INITIAL;
+    static std::array<std::byte, 16> packetBuffer = {};
+    static size_t bufIndex = 0;
+
+    packetBuffer[bufIndex] = byte;
+
+    const auto byteN = static_cast<uint8_t>(byte);
+    bool parsingError = false;
+    switch (state) {
+        case PacketParseState::INITIAL:
+            if (byteN == 0xAA) state = PacketParseState::FIRST_MAGIC;
+            parsingError = true;
+            break;
+
+        case PacketParseState::FIRST_MAGIC:
+            if (byteN == 0x55) state = PacketParseState::SECOND_MAGIC;
+            parsingError = true;
+            break;
+
+        case PacketParseState::SECOND_MAGIC:
+            if (byteN >= UART_PACKETS_COUNT) {
+                parsingError = true;
+                break;
+            }
+
+            switch (static_cast<UartPacketTypes>(byte)) {
+                case UartPacketTypes::INVALID:
+                default:
+                    parsingError = true;
+                    break;
+            }
+            break;
+
+        default:
+            break;
+    }
+
+    if (parsingError) {
+        raiseError();
+    }
+}
 
 [[noreturn]]
 void mainLoop() {
     for (;;) {
         if (auto byte = queue.pop()) {
-            // LL_USART_TransmitData8(USART2, 'B');
-            LL_USART_TransmitData8(USART2, static_cast<uint8_t>(byte.value()));
+            parsePacket(byte.value());
+        }
+
+        if (error) {
+            error = false;
+            const auto errorPacket = UartPacketError();
+            // Игнорируем результат, потому что если всё пошло не так - всё равно не сможем сообщить об ошибке
+            static_cast<void>(sendPacket(errorPacket));
         }
     }
 }
